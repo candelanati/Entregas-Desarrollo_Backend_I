6c08150605c6a3db1d802bfc116ac49125d59708
candelanati <candelanati23@gmail.com>
2025-03-16 20:21:21 -0300

    borre lineas comentadas

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 const express = require('express')
-// const {CartManager}=require("./dao/cartManager.js")
 const routerProducts = require("./routes/products.router.js")
 const routerCarts = require("./routes/carts.router.js")
 
-// const cartManager = new CartManager("./src/data/cart.json")
 
 const app = express()
 
@@ -21,9 +19,6 @@ app.get("/", (req, res)=>{
     res.setHeader('Content-Type','text/plain')
     res.status(200).send('Home page')
 })
-
-
-
 
 //inicializacion del servidor
 app.listen(PORT, ()=>{
