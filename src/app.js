e10819d6861638efeb00a3cdfccc70be5e6282d1
candelanati <candelanati23@gmail.com>
2025-03-16 21:05:59 -0300

    implementado handlebars - creadas carpetas: views / layout y archivo: main.handlebars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
+const {engine}=require('express-handlebars')
 const routerProducts = require("./routes/products.router.js")
 const routerCarts = require("./routes/carts.router.js")
-
 
 const app = express()
 
@@ -10,10 +10,15 @@ const PORT = 8080
 //Middlewares para poder trabajar con datos JSON 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
 //products
 app.use("/api/products", routerProducts)
 //carts
 app.use("/api/carts",routerCarts)
+
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views","./src/views")
 
 app.get("/", (req, res)=>{
     res.setHeader('Content-Type','text/plain')
